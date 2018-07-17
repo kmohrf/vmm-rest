49833ceffc57623a647e543c0dd7a0e090e30566
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-07-17 21:50:37 +0200

    make handler work with current unmodified vmm v0.7.x branch

--- vmmrest/handler.py
+++ vmmrest/handler.py
@@ -1,7 +1,24 @@
-from VirtualMailManager.handler import Handler
+import os
+from VirtualMailManager.config import Config as Cfg
+from VirtualMailManager.errors import NotRootError
+from VirtualMailManager import handler
+
+# monkey-patch config search paths
+handler.CFG_PATH = ':'.join([
+    os.path.join(os.path.expanduser('~'), '.config', 'vmm'),
+    handler.CFG_PATH
+])
 
 
-class RestHandler(Handler):
+class RestHandler(handler.Handler):
     def __init__(self):
-        super().__init__(skip_root_check=True, skip_some_checks=True)
+        try:
+            super().__init__(skip_some_checks=True)
+        except NotRootError:
+            # we don’t care, but do everything that super().__init__ would have
+            # done hadn’t it thrown the exception.
+            # TODO: maybe implement something like skip_root_check keyword-arg in Handler?
+            if self._check_cfg_file():
+                self._cfg = Cfg(self._cfg_fname)
+                self._cfg.load()
         self.cfg_install()
