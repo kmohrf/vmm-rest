32316686eb8954357655547ca31ac8e3173d3698
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2018-07-18 13:36:42 +0200

    make sure vmm handler is initialized just once
    the vmm handler modifies the builtins module and asserts that it does
    this just once. so we need to make sure that we only instantiate
    RestHandler once.

--- vmmrest/app.py
+++ vmmrest/app.py
@@ -2,14 +2,21 @@ from flask import Flask, g
 from vmmrest.api import api_blueprint
 from vmmrest.handler import RestHandler
 
+
+__HANDLER = None
+
 app = Flask(__name__)
 app.register_blueprint(api_blueprint, url_prefix='/api')
 
 
-@app.before_request
-def _initialize_handler():
-    g.handler = RestHandler()
-
-
 if __name__ == '__main__':
+    @app.before_first_request
+    def _initialize_handler():
+        global __HANDLER
+        __HANDLER = RestHandler()
+
+    @app.before_request
+    def _add_handler_to_scope():
+        g.handler = __HANDLER
+
     app.run()
